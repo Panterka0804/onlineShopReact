cfad472e777cdb5289f0ff56fb5cb229d56be277
Panterka0804 <Darya.Smirnova0804@yandex.ru>
2024-09-19 20:42:09 +0300

    Clear project directory and add components
    Clear projectin public and src, add directory "components" with edded components Header and Footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <div>
+                <span className="logo">Название магазина</span>
+            </div>
+            <div className="presentation"></div>
+        </header>
+    );
+}
