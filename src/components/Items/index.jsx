ed0680fdc1a984172559d567b0fd7e5bdc66f9ad
Panterka0804 <Darya.Smirnova0804@yandex.ru>
2024-09-24 20:37:01 +0300

    add basket, styles, modify header, styles

--- src/components/Items/index.jsx
+++ src/components/Items/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Item from "./../Item";
-import styles from "./Items.module.scss"
+import Item from "../Item";
+import styles from "./Items.module.scss";
 
 export default function Items(props){
     return(
         <main>
             {props.allItems.map(el=>(
-                <Item key={el.id} item={el}/>
+                <Item key={el.id} item={el} onAdd={props.onAdd}/>
             ))}
         </main>
     );
